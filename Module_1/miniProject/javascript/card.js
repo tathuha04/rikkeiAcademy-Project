f7f1ecae0fd2d695dfd0ec1cae178b40762f31fb
LeHoang <lehoang999113@gmail.com>
2022-12-06 11:50:04 +0700

    update

--- Module_1/miniProject/javascript/card.js
+++ Module_1/miniProject/javascript/card.js
@@ -4,6 +4,8 @@ var listProduct = JSON.parse(selectedProduct);
 
 
 function onLoadProducts() {
+    var selectedProduct = localStorage.getItem("selectedProduct");
+    var listProduct = JSON.parse(selectedProduct);
     for (const index in listProduct) {
         console.log(listProduct[index]);
         let product = `
@@ -13,8 +15,11 @@ function onLoadProducts() {
                 <h3>${listProduct[index].name}</h3>
                 <br>
                 <div>
-                    <h3 class="price">Giá ưu đãi:&ensp; ${listProduct[index].price}</h3>
-                </div> <br><br>
+                    <h4 class="quantity">Số lượng:&ensp; ${listProduct[index].quantity}</h4><br>
+                    <h4>Giá ưu đãi:&ensp; ${listProduct[index].newPrice}</h4>
+                    <br/><hr><br/>
+                    <h3 class="price">Thành tiền:&ensp; ${listProduct[index].price}</h3>
+                </div> <br>
                 <button class="btn-buyNow" onclick="buyNow()">Đặt hàng</button>
                 <button class="btn-delete" data-name="${listProduct[index].name}" onclick="deleteProduct(this)">Xóa sản phẩm</button>
             </div>
@@ -27,22 +32,30 @@ onLoadProducts();
 
 function deleteProduct(value) {
     console.log("click");
-    for (let index = 0; index < listProduct.length; index++) {
-        if (listProduct[index].name === value.dataset.name) {
-            listProduct.splice(index, 1);
+    var selectedProduct = localStorage.getItem("selectedProduct");
+    if (selectedProduct == null) {
+        selectedProduct = []
+        localStorage.setItem("selectedProduct", JSON.stringify(listProduct));
+    } else {
+        var listProduct = JSON.parse(selectedProduct);
+        for (let index = 0; index < listProduct.length; index++) {
+            if (listProduct[index].name === value.dataset.name) {
+                listProduct.splice(index, 1);
+            }
         }
+        localStorage.setItem("selectedProduct", JSON.stringify(listProduct));
+        console.log(JSON.stringify(listProduct));
+        window.location.reload();
     }
-    localStorage.setItem("selectedProduct", JSON.stringify(listProduct));
-    console.log(JSON.stringify(listProduct));
-    window.location.reload();
+
 }
 
 function buyNow() {
     let listUser = localStorage.getItem("user");
     if (listUser == null) {
         window.location.href = "../pages/loginForm.html"
-    }else{
-        window.location.href ="../pages/thanhToan.html";
+    } else {
+        window.location.href = "../pages/thanhToan.html";
     }
 
 }
--- Module_1/miniProject/javascript/products.js
+++ Module_1/miniProject/javascript/products.js
@@ -1,77 +1,98 @@
-var furniture = {
-    products: [
-        {
-            name: "Bộ ghế sofa góc da cao cấp – SF10",
-            oldPrice: "80.000.000",
-            newPrice: "60.000.000",
-            image: "../images/sofa_1.jpg"
-        },
-        {
-            name: "Ghế sofa văng nhiều màu hiện đại – SF213",
-            oldPrice: "50.000.000",
-            newPrice: "46.000.000",
-            image: "../images/sofa_2.jpg"
-        },
-        {
-            name: "Bộ sofa da thông minh – SFDK87",
-            oldPrice: "200.000.000",
-            newPrice: "180.000.000",
-            image: "../images/sofa_3.jpg"
-        },
-        {
-            name: "Bộ ghế sofa da nhập khẩu tân cổ điển – SF13",
-            oldPrice: "120.000.000",
-            newPrice: "110.000.000",
-            image: "../images/sofa_4.jpg"
-        },
-        {
-            name: "Bộ ghế sofa da nhập khẩu tân cổ điển – SF13",
-            oldPrice: "20.000.000",
-            newPrice: "16.000.000",
-            image: "../images/giuong_1.jpg"
-        },
-        {
-            name: "Giường ngủ viền vàng tinh tế Pula PB05",
-            oldPrice: "10.000.000",
-            newPrice: "8.000.000",
-            image: "../images/giuong_2.jpg"
-        },
-        {
-            name: "Giường ngủ tròn gỗ thịt Pula PB26",
-            oldPrice: "50.000.000",
-            newPrice: "45.000.000",
-            image: "../images/giuong_3.jpg"
-        },
-        {
-            name: "Bộ ghế sofa da nhập khẩu tân cổ điển – SF13",
-            oldPrice: "120.000.000",
-            newPrice: "110.000.000",
-            image: "../images/giuong_4.jpg"
-        },
-        {
-            name: "Bàn ăn mặt đá chân mạ PVD",
-            oldPrice: "13.000.000",
-            newPrice: "11.500.000",
-            image: "../images/ban_1.jpg"
-        },
-        {
-            name: "Bàn ăn Concorde",
-            oldPrice: "7.000.000",
-            newPrice: "5.000.000",
-            image: "../images/ban_2.jpg"
-        },
-        {
-            name: "Bàn ăn Pula BA32",
-            oldPrice: "17.000.000",
-            newPrice: "15.000.000",
-            image: "../images/ban_3.jpg"
-        },
-    ]
-}
+var products = [
+    {
+        name: "Bộ ghế sofa góc da cao cấp – SF10",
+        oldPrice: "80.000.000",
+        newPrice: "60.000.000",
+        price: 60000000,
+        image: "../images/sofa_1.jpg",
+        quantity: 1
+    },
+    {
+        name: "Ghế sofa văng nhiều màu hiện đại – SF213",
+        oldPrice: "50.000.000",
+        newPrice: "46.000.000",
+        price: 46000000,
+        image: "../images/sofa_2.jpg",
+        quantity: 1
+    },
+    {
+        name: "Bộ sofa da thông minh – SFDK87",
+        oldPrice: "200.000.000",
+        newPrice: "180.000.000",
+        price: 180000000,
+        image: "../images/sofa_3.jpg",
+        quantity: 1
+    },
+    {
+        name: "Bộ ghế sofa da nhập khẩu tân cổ điển – SF13",
+        oldPrice: "120.000.000",
+        newPrice: "110.000.000",
+        price: 110000000,
+        image: "../images/sofa_4.jpg",
+        quantity: 1
+    },
+    {
+        name: "Bộ ghế sofa da nhập khẩu tân cổ điển – SF13",
+        oldPrice: "20.000.000",
+        newPrice: "16.000.000",
+        price: "16.000.000",
+        image: "../images/giuong_1.jpg",
+        quantity: 1
+    },
+    {
+        name: "Giường ngủ viền vàng tinh tế Pula PB05",
+        oldPrice: "10.000.000",
+        newPrice: "8.000.000",
+        price: 8000000,
+        image: "../images/giuong_2.jpg",
+        quantity: 1
+    },
+    {
+        name: "Giường ngủ tròn gỗ thịt Pula PB26",
+        oldPrice: "50.000.000",
+        newPrice: "45.000.000",
+        price: 45000000,
+        image: "../images/giuong_3.jpg",
+        quantity: 1
+    },
+    {
+        name: "Bộ ghế sofa da nhập khẩu tân cổ điển – SF13",
+        oldPrice: "120.000.000",
+        newPrice: "110.000.000",
+        price: 110000000,
+        image: "../images/giuong_4.jpg",
+        quantity: 1
+    },
+    {
+        name: "Bàn ăn mặt đá chân mạ PVD",
+        oldPrice: "13.000.000",
+        newPrice: "115.000.000",
+        price: 11500000,
+        image: "../images/ban_1.jpg",
+        quantity: 1
+    },
+    {
+        name: "Bàn ăn Concorde",
+        oldPrice: "7.000.000",
+        newPrice: "5.000.000",
+        price: 5000000,
+        image: "../images/ban_2.jpg",
+        quantity: 1
+    },
+    {
+        name: "Bàn ăn Pula BA32",
+        oldPrice: "17.000.000",
+        newPrice: "15.000.000",
+        price: 15000000,
+        image: "../images/ban_3.jpg",
+        quantity: 1
+    },
+]
+
 
 function onLoadProducts() {
     let renderFurniture = document.getElementById("furniture");
-    let allProducts = furniture.products;
+    let allProducts = products;
     for (let i = 0; i < allProducts.length; i++) {
         // console.log(allProducts[i]);
 
@@ -87,9 +108,9 @@ function onLoadProducts() {
                 </div>
             </div>
             <button class="btn-addCard" data-image="${allProducts[i].image}" data-name="${allProducts[i].name}"
-             data-price="${allProducts[i].newPrice}" onclick="addCard(this)">
-                    <span>Thêm vào giỏ hàng</span>
-             </button>
+                data-newprice="${allProducts[i].newPrice}" data-price="${allProducts[i].price}" data-quantity="${allProducts[i].quantity}"
+                onclick="addCard(this)"> <span>Thêm vào giỏ hàng</span>
+            </button>
         </div>
         `
         renderFurniture.innerHTML += product
@@ -107,7 +128,9 @@ function addCard(product) {
     let objectProduct = {
         image: product.dataset.image,
         name: product.dataset.name,
-        price: product.dataset.price
+        newPrice: product.dataset.newprice,
+        price: product.dataset.price,
+        quantity: product.dataset.quantity,
     }
     let listProduct = localStorage.getItem("selectedProduct");
 
@@ -119,46 +142,45 @@ function addCard(product) {
             .then(() => {
                 window.location.reload();
             })
-    } else {
-        listProduct = JSON.parse(listProduct)
+    } else if (JSON.parse(listProduct).length == '') {
+        listProduct = JSON.parse(listProduct);
         listProduct.push(objectProduct);
         localStorage.setItem("selectedProduct", JSON.stringify(listProduct));
-        // console.log(JSON.stringify(selectedProduct));
+        console.log("đã có sản phẩm này rồi");
         swal("Sản phẩm đã được thêm vào giỏ hàng!", "chúc bạn 1 ngày tốt lành", "success")
             .then(() => {
                 window.location.reload();
             })
+    } else {
+        listProduct = JSON.parse(listProduct);
+        let check = false;
+        for (let i = 0; i < listProduct.length; i++) {
+            if (listProduct[i].name == objectProduct.name) {
+                check = false;
+                let count = Number(listProduct[i].quantity);
+                let price = Number(listProduct[i].price);
+                listProduct[i].quantity = ++count;
+                listProduct[i].price = price + price;
+                localStorage.setItem("selectedProduct", JSON.stringify(listProduct));
+                swal("Sản phẩm đã được thêm vào giỏ hàng!", "chúc bạn 1 ngày tốt lành", "success")
+                    .then(() => {
+                        window.location.reload();
+                    })
+                break;
+            } else {
+                check = true;
+            }
+        }
+        if (check == true) {
+            listProduct.push(objectProduct);
+            localStorage.setItem("selectedProduct", JSON.stringify(listProduct));
+            swal("Sản phẩm đã được thêm vào giỏ hàng!", "chúc bạn 1 ngày tốt lành", "success")
+                .then(() => {
+                    window.location.reload();
+                })
+        } else {
+            console.log("đã có sản phẩm này rồi");
+        }
     }
-
-
 }
 
-
-// function addCard(product) {
-//     console.log("click");
-//     console.log(product.dataset.image);
-//     console.log(product.dataset.name);
-//     console.log(product.dataset.price);
-
-//     let productList = {
-//         image: product.dataset.image,
-//         name: product.dataset.name,
-//         price: product.dataset.price,
-//         quantity: product.dataset.quantity
-//     }
-//     let selectedProduct = JSON.parse(localStorage.getItem("selectedProduct")) || [];
-
-//     if (selectedProduct.length >= 1) {
-//         const productSelected = selectedProduct.find(item => item.name == productList.name);
-//         if (Boolean(productSelected)) {
-//            productSelected.quantity += productList.quantity
-//         } else {
-//             selectedProduct.push(productList);
-//         }
-//     } else {
-//         selectedProduct.push(productList);
-//     }
-//     console.log(selectedProduct);
-//     localStorage.setItem("selectedProduct", JSON.stringify(selectedProduct));
-//     console.log(JSON.stringify(selectedProduct));
-// }
